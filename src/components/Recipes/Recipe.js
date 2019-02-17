0294da5aaa57349b561ae3e55949fc817e044021
Denis Guimaraes <guimaraes.dev@gmail.com>
2019-02-17 17:23:08 +0100

    get recipes from a book and display it

--- src/components/Recipes/Recipe.js
+++ src/components/Recipes/Recipe.js
@@ -1,21 +1,22 @@
 // NPM import
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import { FaFileSignature } from 'react-icons/fa';
+
 // Local import
 
 // Styles
 import './recipes.scss';
 
 // Code
-
-const Recipe = () => (
+const Recipe = ({ recipeData }) => (
   <div className="recipe">
     <div className="recipe-wrapper">
       <div className="recipe-info">
-        <span className="recipe-info__name">Recette: Nom d'une recette</span>
-        <span className="recipe-info__book">Livre: Nom du livre</span>
-        <span className="recipe-info__page"> Page: 122</span>
+        <span className="recipe-info__name">Recette: {recipeData.name}</span>
+        <span className="recipe-info__book">Livre: {recipeData.book.name}</span>
+        <span className="recipe-info__page"> Page: {recipeData.page}</span>
       </div>
       <Link to="/recettes/2/editer" className="recipe-edit">
         <FaFileSignature className="recipe-edit__icon" />Editer
@@ -29,6 +30,16 @@ const Recipe = () => (
     </div>
   </div>
 );
+
+Recipe.propTypes = {
+  recipeData: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    page: PropTypes.number.isRequired,
+    book: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+    }),
+  }).isRequired,
+};
 
 // Export
 export default Recipe;
